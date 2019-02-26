5a16c47a7bdc7d5a8fd8fe77fa45c2b1e1e0519d
adenisovgit <adenisov@gmail.com>
2019-02-26 11:00:20 +0100

    Исправления на шаге 2 по заданию ментора. Так же исправлена версионность.

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 // import _ from 'lodash'
 // console.log(_.capitalize("welcome to the Brain Games!"))
-import { getUserName } from '..'
-console.log("Welcome to the Brain Games!");
-let userName = getUserName();
+import getUserName from '..'
+console.log("Welcome to the Brain Games!")
+getUserName()
