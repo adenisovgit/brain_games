cb64c06e759640e68facbaa4740932464f202931
adenisovgit <adenisov@gmail.com>
2019-02-27 16:16:20 +0100

    Отправка на проверку шаг 4

---
+++ src/bin/brain-even.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+// import _ from 'lodash'
+// console.log(_.capitalize("welcome to the Brain Games!"))
+import { getUserName, quiz } from '..';
+
+getUserName();
+quiz();
--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import readlineSync from 'readline-sync';
 
-const getUserName = () => {
+const stYes = '\x1b[31m"yes"\x1b[0m';
+const stNo = '\x1b[31m"no"\x1b[0m';
+let userName = '';
+
+export const getUserName = () => {
   console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${userName}!`);
+  console.log(`Answer ${stYes} if number even otherwise answer ${stNo}.\n`);
+  userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${userName}!\n`);
 };
 
-export default getUserName;
+export const quiz = () => {
+  let i = 0;
+  while (i < 3) {
+    const num = Math.round(Math.random() * 100);
+    console.log(`Question: ${num}`);
+    const answ = readlineSync.question('Your answer: ');
+    if ((answ === 'no' && num % 2) || (answ === 'yes' && !(num % 2))) {
+      console.log('Correct!');
+      i += 1;
+    } else {
+      const st2 = (num % 2) ? stNo : stYes;
+      console.log(`\x1b[31m${answ}\x1b[0m is wrong answer ;(. Correct answer was ${st2}.\n Let\x1b[31m's try again, ${userName}!\x1b[0m`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!\n`);
+};
